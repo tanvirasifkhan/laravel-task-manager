700d35fa79cd8785b0527614b9b1121640b1348c
Asif Khan <asifkhan.github@gmail.com>
2018-09-17 11:55:59 +0600

    file modified

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -4,7 +4,7 @@
         <div class="my-3 my-md-5">
             <div class="container">
               <div class="page-header">
-                <h1 class="page-title">Ongoing Projects</h1> 
+                <h1 class="page-title">My Projects</h1> 
                 <div class="row gutters-xs ml-auto">
                     <div class="col">
                         <input type="text" class="form-control" placeholder="Search for...">
