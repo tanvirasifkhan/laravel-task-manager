de10746a152690ee98ea79d5d458770d1713d5d8
Asif Khan <asifkhan.github@gmail.com>
2018-09-17 11:20:47 +0600

    bootstrap javascript and css cdn link added

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -18,12 +18,9 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
-    <script src="{{asset('js/require.min.js')}}"></script>
-    <script>
-      requirejs.config({
-          baseUrl: '.'
-      });
-    </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <!-- Dashboard Core -->
     <link href="{{asset('css/dashboard.css')}}" rel="stylesheet" />
     <script src="{{asset('js/dashboard.js')}}"></script>
