6f5e30ded3b50f9b835a3d5adad20aff12081687
Asif Khan <asifkhan.github@gmail.com>
2018-09-18 22:44:00 +0600

    completed tasks shown in a table

--- resources/views/completed_tasks.blade.php
+++ resources/views/completed_tasks.blade.php
@@ -11,51 +11,34 @@
                   <div class="card">
                     <div class="table-responsive">
                         <table class="table card-table table-vcenter text-nowrap">
-                            <thead>
+                          <thead>
+                            <tr>
+                              <th>Task Title</th>
+                              <th>Category</th>
+                              <th>Status</th>
+                              <th>Created At</th>
+                              <th>Start Date</th>
+                              <th>End Date</th>
+                              <th>Description</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            @foreach($completed as $task)
                               <tr>
-                                <th>Task Title</th>
-                                <th>Category</th>
-                                <th>Status</th>
-                                <th>Created At</th>
-                                <th>Start Date</th>
-                                <th>End Date</th>
-                                <th>Action</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td>Design Works</td>
-                                <td>Carlson Limited</td>
+                                <td>{{$task->title}}</td>
+                                <td>{{$task->category->category_title}}</td>
                                 <td><p class="text-center text-light bg-success m-2">Done</p></td>
-                                <td>87956621</td>
-                                <td>15 Dec 2017</td>
-                                <td><span class="status-icon bg-success"></span> Paid</td>
-                                <td>
-                                  <a href="" class="btn btn-success">Done</a>
-                                  <a href="" class="btn btn-warning">Pending</a>
-                                  <a href="" class="btn btn-info">Edit</a>
-                                  <a href="" class="btn btn-danger">Remove</a>
-                                </td>
+                                <td>{{date_format(date_create($task->created_at),'d M,Y')}}</td>
+                                <td>{{date_format(date_create($task->start_date),'d M,Y')}}</td>
+                                <td>{{date_format(date_create($task->end_date),'d M,Y')}}</td>
+                                <td>{{$task->description}}</td>
                               </tr>
-                            </tbody>
+                            @endforeach
+                          </tbody>
                           </table>
                     </div>
                   </div>
-                  <ul class="pagination justify-content-end">
-                      <li class="page-item page-prev disabled">
-                        <a class="page-link" href="#" tabindex="-1">Prev</a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#">5</a></li>
-                      <li class="page-item page-next">
-                        <a class="page-link" href="#">
-                          Next
-                        </a>
-                      </li>
-                    </ul>
+                  {{$completed->links()}}
                 </div>
               </div>              
             </div>            
