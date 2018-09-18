08fa86d7bdd26768ab8d8261bc864c6286efa83b
Asif Khan <asifkhan.github@gmail.com>
2018-09-18 22:43:40 +0600

    modified

--- resources/views/pending_tasks.blade.php
+++ resources/views/pending_tasks.blade.php
@@ -38,21 +38,7 @@
                           </table>
                     </div>
                   </div>
-                  <ul class="pagination justify-content-end">
-                      <li class="page-item page-prev disabled">
-                        <a class="page-link" href="#" tabindex="-1">Prev</a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#">5</a></li>
-                      <li class="page-item page-next">
-                        <a class="page-link" href="#">
-                          Next
-                        </a>
-                      </li>
-                    </ul>
+                  {{$pendings->links()}}
                 </div>
               </div>              
             </div>            
