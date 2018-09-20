2191eab79b9a9d6714b13929ba2ecf2cd5b4e4e9
Asif Khan <asifkhan.github@gmail.com>
2018-09-21 04:29:04 +0600

    form validation and store route added

--- resources/views/add_project.blade.php
+++ resources/views/add_project.blade.php
@@ -10,31 +10,50 @@
                 <div class="col-12">
                   <div class="card p-4">
                       <div class="card-body">
-                            <form action="" method="POST">
+                            <form action="{{route('project.store')}}" method="POST">
+                                @csrf
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Enter project title...">
+                                    <input type="text" class="form-control {{($errors->has('project_title'))?'is-invalid':''}}" value="{{old('project_title')}}" name="project_title" placeholder="Enter project title...">
+                                    @if($errors->has('project_title'))
+                                       <p class="text-danger">{{$errors->first('project_title')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
-                                    <select name="project_category" class="form-control">
-                                      <option value="">Choose a category</option>
+                                    <select name="project_category" class="form-control  {{($errors->has('project_category'))?'is-invalid':''}}">
+                                        <option value="">Choose a category</option>
+                                        @foreach($categories as $category)
+                                           <option value="{{$category->id}}" {{(old('project_category')==$category->id)?'selected':''}}>{{$category->category_title}}</option>
+                                        @endforeach
                                     </select>
+                                    @if($errors->has('project_category'))
+                                       <p class="text-danger">{{$errors->first('project_category')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
                                     <label for="recipient-name" class="col-form-label">Start From</label>
-                                    <input type="date" class="form-control" name="start_date" placeholder="Enter start date...">
+                                    <input type="date" class="form-control  {{($errors->has('start_date'))?'is-invalid':''}}" name="start_date" value="{{old('start_date')}}" placeholder="Enter start date...">
+                                    @if($errors->has('start_date'))
+                                       <p class="text-danger">{{$errors->first('start_date')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
                                     <label for="recipient-name" class="col-form-label">End date</label>
-                                    <input type="date" class="form-control" name="end_date" placeholder="Enter end date...">
+                                    <input type="date" class="form-control  {{($errors->has('end_date'))?'is-invalid':''}}" name="end_date" value="{{old('end_date')}}" placeholder="Enter end date...">
+                                    @if($errors->has('end_date'))
+                                       <p class="text-danger">{{$errors->first('end_date')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
-                                    <textarea class="form-control" name="description" placeholder="Enter project description..."></textarea>
+                                    <textarea class="form-control {{($errors->has('description'))?'is-invalid':''}}" name="description" placeholder="Enter project description...">{{old('description')}}</textarea>
+                                    @if($errors->has('description'))
+                                       <p class="text-danger">{{$errors->first('description')}}</p>
+                                    @endif
                                 </div>
                                 <div class="card-footer float-right">
-                                    <a href="{{route('project.ongoing')}}" class="btn btn-danger">Cancel</a>
+                                    <a href="{{route('project.all')}}" class="btn btn-danger">Cancel</a>
                                     <button type="submit" class="btn btn-success">Save</button>
                                 </div>
-                                </form>
+                            </form>
                       </div>
                   </div>
                 </div>
