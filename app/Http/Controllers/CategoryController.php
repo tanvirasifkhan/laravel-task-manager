51cdbbcd2e199afbbefcb3129b14751f1b9b1211
Asif Khan <asifkhan.github@gmail.com>
2018-09-20 18:14:21 +0600

    all categories fetched

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Categories;
 
 class CategoryController extends Controller
 {
@@ -13,7 +14,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('categories');
+        $all_categories=Categories::all();
+        return view('categories',['categories'=>$all_categories]);
     }
 
     /**
