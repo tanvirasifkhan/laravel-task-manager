366985e09cf6e0609c06e9674cb0c7606e66ffee
Asif Khan <asifkhan.github@gmail.com>
2018-09-21 05:17:19 +0600

    completed projects shown

--- resources/views/completed_projects.blade.php
+++ resources/views/completed_projects.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-12">
                   <div class="card p-4">
                     <div class="table-responsive">
-                        <table id="example" class="table card-table table-striped table-vcenter text-nowrap">
+                        <table id="example" class="table table-striped card-table table-vcenter text-nowrap">
                             <thead>
                               <tr>
                                 <th>Project Title</th>
@@ -19,24 +19,68 @@
                                 <th>Created At</th>
                                 <th>Start Date</th>
                                 <th>End Date</th>
+                                <th>Description</th>
                                 <th>Action</th>
                               </tr>
                             </thead>
                             <tbody>
-                              <tr>
-                                <td>Design Works</td>
-                                <td>Carlson Limited</td>
-                                <td><p class="text-center text-light bg-success m-2">Done</p></td>
-                                <td>87956621</td>
-                                <td>15 Dec 2017</td>
-                                <td><span class="status-icon bg-success"></span> Paid</td>
-                                <td>
-                                  <a href="" class="btn btn-success">Done</a>
-                                  <a href="" class="btn btn-warning">Pending</a>
-                                  <a href="" class="btn btn-info">Edit</a>
-                                  <a href="" class="btn btn-danger">Remove</a>
-                                </td>
-                              </tr>
+                              @foreach($projects as $project)
+                                <tr>
+                                  <td>{{$project->title}}</td>
+                                  <td>{{$project->category->category_title}}</td>
+                                  <td>
+                                      @if($project->status=='pending')
+                                        <p class="text-center text-light bg-warning m-2 p-1">Pending</p>
+                                      @else
+                                        <p class="text-center text-light bg-success m-2">Done</p>
+                                      @endif
+                                  </td>
+                                  <td>{{date_format(date_create($project->created_at),'d M,Y')}}</td>
+                                  <td>{{date_format(date_create($project->start_date),'d M,Y')}}</td>
+                                  <td>{{date_format(date_create($project->end_date),'d M,Y')}}</td>
+                                  <td>{{$project->description}}</td>
+                                  <td>
+                                     @if($project->status=='pending')
+                                      <a href="" class="btn btn-success" onclick="
+                                        if(confirm('Are you sure ?')){
+                                          event.preventDefault();
+                                          document.getElementById('make_completed-{{$project->id}}').submit();
+                                        }else{
+                                          event.preventDefault();
+                                        }
+                                      ">Mark Completed</a>
+                                     @elseif($project->status=='completed')
+                                      <a href="" class="btn btn-warning" onclick="
+                                        if(confirm('Are you sure ?')){
+                                          event.preventDefault();
+                                          document.getElementById('make_pending-{{$project->id}}').submit();
+                                        }else{
+                                          event.preventDefault();
+                                        }
+                                    ">Mark Pending</a>
+                                     @endif
+                                    <a href="{{route('project.edit',$project->id)}}" class="btn btn-info">Edit</a>
+                                    <a href="" class="btn btn-danger" onclick="
+                                       if(confirm('Are you sure ?')){
+                                         event.preventDefault();
+                                         document.getElementById('delete-{{$project->id}}').submit();
+                                       }else{
+                                         event.preventDefault();
+                                       }
+                                    ">Remove</a>
+                                    <form id="delete-{{$project->id}}" style="display:none;" action="{{route('project.delete',$project->id)}}" method="POST">
+                                      @csrf
+                                      @method('delete')
+                                    </form>
+                                    <form id="make_completed-{{$project->id}}" style="display:none;" action="{{route('project.make_completed',$project->id)}}" method="POST">
+                                      @csrf
+                                    </form>
+                                      <form id="make_pending-{{$project->id}}" style="display:none;" action="{{route('project.make_pending',$project->id)}}" method="POST">
+                                        @csrf
+                                      </form>
+                                  </td>
+                                </tr>
+                              @endforeach
                             </tbody>
                           </table>
                     </div>
