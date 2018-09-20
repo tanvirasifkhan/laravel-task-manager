feacf8134756c697b4b978e8b3bb2330cbcb1139
Asif Khan <asifkhan.github@gmail.com>
2018-09-21 04:42:48 +0600

    relationship model created

--- app/Categories.php
+++ app/Categories.php
@@ -4,11 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Tasks;
+use App\Projects;
 
 class Categories extends Model{
     protected $fillable=['category_title'];
 
     public function tasks(){
         return $this->hasMany(Tasks::class);
+    }
+
+    public function project(){
+        return $this->hasMany(Projects::class);
     }
 }
--- app/Projects.php
+++ app/Projects.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Categories;
 
 class Projects extends Model
 {
     protected $fillable=['title','category_id','start_date','end_date','description','status'];
+
+    public function category(){
+        return $this->belongsTo(Categories::class);
+    }
 }
