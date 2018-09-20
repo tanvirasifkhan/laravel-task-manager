d1fc6b0a4644b76a6329c063b6861605d9c85342
Asif Khan <asifkhan.github@gmail.com>
2018-09-20 18:14:52 +0600

    categories fetached and shown

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -7,11 +7,8 @@
                 <h1 class="page-title">Categories</h1> 
                 <div class="row gutters-xs ml-auto">
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="Search for...">
+                        <a href="{{route('category.create')}}" class="btn btn-success">Create Category</a>
                     </div>
-                    <span class="col-auto">
-                        <button class="btn btn-secondary" type="button"><i class="fe fe-search"></i></button>
-                    </span>
                 </div>
               </div>
               <div class="row row-cards row-deck">
@@ -23,42 +20,24 @@
                           <tr>
                             <th>Category Title</th>
                             <th>Created At</th>
-                            <th>Updated At</th>
                             <th>Action</th>
-                            <th><a href="{{route('category.create')}}" class="btn btn-success">Create Category</a></th>
                           </tr>
                         </thead>
                         <tbody>
+                          @foreach($categories as $category)
                           <tr>
-                            <td>Design Works</td>
-                            <td>15 Dec 2017</td>
-                            <td>15 Dec 2017</td>
+                            <td>{{$category->category_title}}</td>
+                            <td>{{date_format(date_create($category->created_at),'d M,Y')}}</td>
                             <td>
                               <a href="" class="btn btn-info">Edit</a>
                               <a href="" class="btn btn-danger">Remove</a>
                             </td>
                           </tr>
+                          @endforeach
                         </tbody>
                       </table>
                     </div>
                   </div>
-                  <ul class="pagination justify-content-end">
-                      <li class="page-item page-prev disabled">
-                        <a class="page-link" href="#" tabindex="-1">
-                          Prev
-                        </a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#">5</a></li>
-                      <li class="page-item page-next">
-                        <a class="page-link" href="#">
-                          Next
-                        </a>
-                      </li>
-                    </ul>
                 </div>
               </div>              
             </div>            
