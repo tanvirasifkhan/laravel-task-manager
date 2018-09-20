e968dc8717907dea6d6d0aecad0d428df41f295b
Asif Khan <asifkhan.github@gmail.com>
2018-09-21 01:38:42 +0600

    form validation and store route added

--- resources/views/add_task.blade.php
+++ resources/views/add_task.blade.php
@@ -1,5 +1,5 @@
 @extends('master.app')
-@section('title','Ongoing Tasks')
+@section('title','Create New Task')
 @section('content')          
         <div class="my-3 my-md-5">
             <div class="container">
@@ -12,30 +12,44 @@
                         <div class="card-status bg-green"></div>
                         <div class="card-header">Add Task</div>
                         <div class="card-body">
-                           <form action="" method="POST">
+                           <form action="{{route('task.store')}}" method="POST">
                                 @csrf
                                 <div class="form-group">
-                                    <input type="text" name="task_title" class="form-control" id="recipient-name" placeholder="Enter title..."> 
-                                    <p class="errorTitle text-danger"></p>
+                                    <input type="text" name="task_title" class="form-control {{($errors->has('task_title'))?'is-invalid':''}}" value="{{old('task_title')}}" placeholder="Enter task title..."> 
+                                    @if($errors->has('task_title'))
+                                       <p class="text-danger">{{$errors->first('task_title')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
-                                    <select name="task_category" id="" class="form-control">
+                                    <select name="task_category" id="" class="form-control {{($errors->has('task_category'))?'is-invalid':''}}">
                                     <option value="">Choose a category</option>
-                                    @foreach($categories as $category)
-                                        <option value="{{$category->id}}">{{$category->category_title}}</option>
-                                    @endforeach
+                                        @foreach($categories as $category)
+                                            <option value="{{$category->id}}" {{(old('task_category')==$category->id)?'selected':''}}>{{$category->category_title}}</option>
+                                        @endforeach
                                     </select>
+                                    @if($errors->has('task_category'))
+                                       <p class="text-danger">{{$errors->first('task_category')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
                                     <label for="recipient-name" class="col-form-label">Start From</label>
-                                    <input type="date" name="start_date" class="form-control" id="recipient-name">
+                                    <input type="date" name="start_date" value="{{old('start_date')}}" class="form-control {{($errors->has('start_date'))?'is-invalid':''}}" placeholder="Enter start date...">
+                                    @if($errors->has('start_date'))
+                                       <p class="text-danger">{{$errors->first('start_date')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
                                     <label for="recipient-name" class="col-form-label">End date</label>
-                                    <input type="date" name="end_date" class="form-control" id="recipient-name">
+                                    <input type="date" name="end_date" value="{{old('end_date')}}" class="form-control {{($errors->has('start_date'))?'is-invalid':''}}" placeholder="Enter end date...">
+                                    @if($errors->has('end_date'))
+                                       <p class="text-danger">{{$errors->first('end_date')}}</p>
+                                    @endif
                                 </div>
                                 <div class="form-group">
-                                    <textarea class="form-control" name="description" id="message-text" placeholder="Task description"></textarea>
+                                    <textarea class="form-control {{($errors->has('description'))?'is-invalid':''}}" name="description" placeholder="Task description...">{{old('description')}}</textarea>
+                                    @if($errors->has('description'))
+                                       <p class="text-danger">{{$errors->first('description')}}</p>
+                                    @endif
                                 </div>                  
                                 <div class="card-footer float-right">
                                     <a href="{{route('task.ongoing')}}" class="btn btn-danger">Cancel</a>
