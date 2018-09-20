a30f4773bb77a274f2f8fd6b1a6f78fb98420855
Asif Khan <asifkhan.github@gmail.com>
2018-09-21 04:43:39 +0600

    projects data shown on the view

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -27,24 +27,37 @@
                                 <th>Created At</th>
                                 <th>Start Date</th>
                                 <th>End Date</th>
+                                <th>Description</th>
                                 <th>Action</th>
                               </tr>
                             </thead>
                             <tbody>
-                              <tr>
-                                <td>Design Works</td>
-                                <td>Carlson Limited</td>
-                                <td><p class="text-center text-light bg-success m-2">Done</p></td>
-                                <td>87956621</td>
-                                <td>15 Dec 2017</td>
-                                <td><span class="status-icon bg-success"></span> Paid</td>
-                                <td>
-                                  <a href="" class="btn btn-success">Done</a>
-                                  <a href="" class="btn btn-warning">Pending</a>
-                                  <a href="" class="btn btn-info">Edit</a>
-                                  <a href="" class="btn btn-danger">Remove</a>
-                                </td>
-                              </tr>
+                              @foreach($projects as $project)
+                                <tr>
+                                  <td>{{$project->title}}</td>
+                                  <td>{{$project->category->category_title}}</td>
+                                  <td>
+                                      @if($project->status=='pending')
+                                        <p class="text-center text-light bg-warning m-2 p-1">Pending</p>
+                                      @else
+                                        <p class="text-center text-light bg-success m-2">Done</p>
+                                      @endif
+                                  </td>
+                                  <td>{{date_format(date_create($project->created_at),'d M,Y')}}</td>
+                                  <td>{{date_format(date_create($project->start_date),'d M,Y')}}</td>
+                                  <td>{{date_format(date_create($project->end_date),'d M,Y')}}</td>
+                                  <td>{{$project->description}}</td>
+                                  <td>
+                                     @if($project->status=='pending')
+                                      <a href="" class="btn btn-success">Mark Done</a>
+                                     @elseif($project->status=='completed')
+                                      <a href="" class="btn btn-warning">Mark Pending</a>
+                                     @endif
+                                    <a href="" class="btn btn-info">Edit</a>
+                                    <a href="" class="btn btn-danger">Remove</a>
+                                  </td>
+                                </tr>
+                              @endforeach
                             </tbody>
                           </table>
                     </div>
