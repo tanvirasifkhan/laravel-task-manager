307e3cb6e0c128ad0177c1ac9ee4b4a65c6c5b90
Asif Khan <asifkhan.github@gmail.com>
2018-09-20 19:50:01 +0600

    new tamplate created for edit category

---
+++ resources/views/edit_category.blade.php
@@ -0,0 +1,37 @@
+@extends('master.app')
+@section('title','Edit Category')
+@section('content')          
+        <div class="my-3 my-md-5">
+            <div class="container">
+              <div class="page-header d-flex justify-content-center">
+                <h1 class="page-title">Edit Category</h1> 
+              </div>
+              <div class="row row-cards row-deck d-flex justify-content-center">
+                <div class="col-6">
+                  <div class="card">
+                    <div class="card-status bg-green"></div>
+                    <div class="card-header">Edit Category</div>
+                    <div class="card-body">
+                        @foreach($category as $each)
+                        <form action="{{route('category.store')}}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" name="category_title" class="form-control {{($errors->has('category_title'))?'is-invalid':''}}" value="{{$each->category_title}}" placeholder="Enter category title..."> 
+                                @if($errors->has('category_title'))
+                                   <p class="text-danger">{{$errors->first('category_title')}}</p>
+                                @endif
+                            </div>             
+                            <div class="card-footer float-right">
+                                <a href="{{route('category.index')}}" class="btn btn-danger">Cancel</a>
+                                <button type="submit" class="btn btn-success">Save</button>
+                            </div>
+                        </form>
+                        @endforeach
+                    </div>
+                  </div>
+                </div>
+              </div>              
+            </div>            
+          </div>
+      </div>
+    @endsection
