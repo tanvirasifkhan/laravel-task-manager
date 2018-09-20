b0ee08883d541de91c7df6914a44d562daa1c83a
Asif Khan <asifkhan.github@gmail.com>
2018-09-21 03:56:04 +0600

    modal removed and file modified

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -7,7 +7,7 @@
                 <h1 class="page-title">My Projects</h1> 
                 <div class="row gutters-xs ml-auto">
                     <div class="col">
-                        <a href="" class="btn btn-success">Create Project</a>
+                        <a href="{{route('project.create')}}" class="btn btn-success">Create Project</a>
                     </div>
                 </div>
               </div>
@@ -51,41 +51,4 @@
             </div>            
           </div>
       </div>
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Create New Project</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <form action="" method="POST">
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Enter title...">
-                  </div>
-                  <div class="form-group">
-                    <select name="" id="" class="form-control">
-                      <option value="">Choose a category</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="recipient-name" class="col-form-label">Start From</label>
-                    <input type="date" class="form-control" id="recipient-name">
-                  </div>
-                  <div class="form-group">
-                      <label for="recipient-name" class="col-form-label">End date</label>
-                      <input type="date" class="form-control" id="recipient-name">
-                    </div>
-                  <div class="form-group">
-                    <textarea class="form-control" id="message-text" placeholder="Project description"></textarea>
-                  </div>
-                </form>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-success">Save</button>
-              </div>
-            </div>
-          </div>
-        </div>
     @endsection
