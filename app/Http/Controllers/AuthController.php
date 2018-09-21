2ae678339150e72b378a23114881c541e77dbe15
Asif Khan <asifkhan.github@gmail.com>
2018-09-21 06:02:23 +0600

    authenticate method created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     // show the login form
     public function login(){
         return view('auth.login');
+    }
+
+    // authenticate the user
+    public function authenticate(Request $request){
+        $validators=Validator::make($request->all(),[
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        if($validators->fails()){
+            return redirect()->route('auth.login')->withErrors($validators)->withInput();
+        }else{
+            $email=$request->email;
+            $password=$request->password;
+
+            if(Auth::attempt(['email' => $email, 'password' => $password])){
+                return redirect()->intended('/');
+            }else{
+                return redirect()->route('auth.login')->with('message','Email/Password is incorrect !');
+            }
+        }
     }
 }
