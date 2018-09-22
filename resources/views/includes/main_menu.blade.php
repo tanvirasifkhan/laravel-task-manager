568c43379a3a7229616cbeafda508121e1dbd996
Asif Khan <asifkhan.github@gmail.com>
2018-09-23 04:15:51 +0600

    route link added

--- resources/views/includes/main_menu.blade.php
+++ resources/views/includes/main_menu.blade.php
@@ -25,7 +25,7 @@
             </div>
         </li>
         <li class="nav-item">
-            <a href="" class="nav-link"><i class="fe fe-box"></i> Project Tasks</a>
+            <a href="{{route('project_task.all')}}" class="nav-link"><i class="fe fe-box"></i> Project Tasks</a>
         </li>
         </ul>
     </div>
